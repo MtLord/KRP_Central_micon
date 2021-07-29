e844b7c5083fa1ad03cd7879db911227ff51de27
MtLord <a217018@ns.kogakuin.ac.jp>
2021-07-29 21:15:04 +0900

    ブラシレスモータを2つ追加した

--- Inc/Libraries/LowlayerHandel.hpp
+++ Inc/Libraries/LowlayerHandel.hpp
@@ -35,7 +35,7 @@ public:
 
 	/*************************/
 
-	Motor SM1,SM2,SM3,SM4,SM5,SM6,SM7,SM8,BR1,BR2;
+	Motor SM1,SM2,SM3,SM4,SM5,SM6,SM7,SM8,BR1,BR2,BR3,BR4;
 	MotorSystem M1,M2,M3,M4,M5,M6,M7,M8;
 	Aircylinder Air1,Air2,Air3,Air4,Air5,Air6,Air7,Air8;
 	localization loca;
@@ -45,7 +45,7 @@ public:
 	SteppingMotor Stepper1,Stepper2,Stepper3;
 	LowlayerHandelTypedef():extcan_d(CAN_ID_EXT,CAN_RTR_DATA),extcan_r(CAN_ID_EXT,CAN_RTR_REMOTE),stdcan_d(CAN_ID_STD,CAN_RTR_DATA)
 	,SM1(&extcan_d,1,1),SM2(&extcan_d,2,1),SM3(&extcan_d,3,1),SM4(&extcan_d,4,1),SM5(&extcan_d,1,2),SM6(&extcan_d,2,2),SM7(&extcan_d,3,2),SM8(&extcan_d,4,2)
-	,BR1(&extcan_d,17,1),BR2(&extcan_d,18,1)
+	,BR1(&extcan_d,17,1),BR2(&extcan_d,18,1),BR3(&extcan_d,17,2),BR4(&extcan_d,18,2)
 	,M1(&stdcan_d,1),M2(&stdcan_d,2),M3(&stdcan_d,3),M4(&stdcan_d,4),M5(&stdcan_d,5),M6(&stdcan_d,6),M7(&stdcan_d,7),M8(&stdcan_d,8)
 	,Air1(&extcan_d,1),Air2(&extcan_d,2),Air3(&extcan_d,3),Air4(&extcan_d,4),Air5(&extcan_d,5),Air6(&extcan_d,6),Air7(&extcan_d,7),Air8(&extcan_d,8)
 	,loca(&extcan_d,&extcan_r)
